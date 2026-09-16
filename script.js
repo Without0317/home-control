0c5b9cd44cfd97f8ef09d82527c8a65df26b3951
Khen Patra <khenpatra0317@gmail.com>
2026-09-16 14:43:31 +0700

    finally got ts working

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const turnonButton = document.getElementById('turnon');
 const turnoffButton = document.getElementById('turnoff');
 
-turnonButton.addEventListener('click', () => {
-    fetch('https://homeswitch.k-680.workers.dev/on')
-});
+async function turnonbutton() {
+    const response = await fetch(
+        "https://homeswitch.k-680.workers.dev/on"
+    );
 
-turnoffButton.addEventListener('click', () => {
-    fetch('https://homeswitch.k-680.workers.dev/off')
-});
+    const result = await response.text();
+
+    console.log(result);
+}
+
+async function turnoffbutton() {
+    const response = await fetch(
+        "https://homeswitch.k-680.workers.dev/on"
+    );
+
+    const result = await response.text();
+
+    console.log(result);
+}
