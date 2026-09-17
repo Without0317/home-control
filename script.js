561f4e49fb8fdccfd01e9a029382df86083410c5
Khen Patra <khenpatra0317@gmail.com>
2026-09-17 18:21:48 +0700

    upgraded UI and added feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 async function turnonbutton() {
     const response = await fetch(
         "https://homeswitch.k-680.workers.dev/on"
@@ -6,6 +7,7 @@ async function turnonbutton() {
     const result = await response.text();
 
     console.log(result);
+    updateStatus();
 }
 
 async function turnoffbutton() {
@@ -16,8 +18,27 @@ async function turnoffbutton() {
     const result = await response.text();
 
     console.log(result);
+    updateStatus();
 }
 
+async function updateStatus() {
+  const statusDiv = document.getElementById("state");
+  statusDiv.textContent = "Status: loading...";
+
+  try {
+    const response = await fetch("https://homeswitch.k-680.workers.dev/status");
+    const result = await response.text();
+    text = result == 1 ? "ON" : "OFF";
+    
+    statusDiv.textContent = "API Status: " + text;
+  } catch (err) {
+    statusDiv.textContent = "API Status: error";
+    console.error("Failed to fetch status:", err);
+  }
+  //setInterval(updateStatus, 5000); // Update status every 5 seconds
+}
+
+updateStatus();
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
